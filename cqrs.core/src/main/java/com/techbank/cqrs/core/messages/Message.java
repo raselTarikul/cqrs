873bb0f16db12417b9616392d3d84301c9cffd90
Tarikul Islam <ti.rasel@gmail.com>
2022-03-26 15:07:44 +0700

    Commands Added

---
+++ cqrs.core/src/main/java/com/techbank/cqrs/core/messages/Message.java
@@ -0,0 +1,14 @@
+package com.techbank.cqrs.core.messages;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class Message {
+    private String id;
+}
