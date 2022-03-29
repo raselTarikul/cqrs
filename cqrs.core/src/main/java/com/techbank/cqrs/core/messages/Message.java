487e368fb9fac93887f2d60792c7b8cc598a9149
Tarikul Islam <ti.rasel@gmail.com>
2022-03-29 22:57:57 +0700

    Aggregate root added

--- cqrs.core/src/main/java/com/techbank/cqrs/core/messages/Message.java
+++ cqrs.core/src/main/java/com/techbank/cqrs/core/messages/Message.java
@@ -4,11 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@SuperBuilder
 public abstract class Message {
     private String id;
 }
